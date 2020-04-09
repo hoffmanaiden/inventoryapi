58a877d95471df3550f9a3f8adcbc4074cf60469
hoffmanaiden <hoffman.aiden@gmail.com>
2020-04-09 16:06:45 -0400

    init
    Todo: create post route for items

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const errorHandler = require("./handlers/error");
+
+const PORT = 8081;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// List of Routes
+
+
+// If no routes can be reached...
+app.use(function(req, res, next){
+  let err = new Error("Route not found");
+  err.status = 404;
+  next(err);
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, function(){
+  console.log(`Server is starting on port ${PORT}`);
+});
