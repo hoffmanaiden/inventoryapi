feb57face3dadcdd1c7b0709ecebb051a0cd12a9
hoffmanaiden <hoffman.aiden@gmail.com>
2020-04-14 14:26:16 -0400

    added updated handler
    Added updated handler for items. Items should now have full CRUD functionality.

--- server/handlers/items.js
+++ server/handlers/items.js
@@ -1,5 +1,6 @@
 const db = require("../models");
 
+// /api/items/
 exports.createItem = async function(req, res, next){
   try{
     let item = await db.Item.create(req.body);
@@ -18,33 +19,18 @@ exports.readItem = async function(req, res, next){
     return next(err);
   }
 }
+// /api/items/:item_id
 exports.updateItem = async function(req, res, next){
   try{
     let item = await db.Item.findById(req.params.item_id);
-    if(req.params.item_id.name){
-      item.name = req.params.item_id.name;
-    }
-    if(req.params.item_id.building){
-      item.building = req.params.item_id.building;
-    }
-    if(req.params.item_id.location){
-      item.location = req.params.item_id.location;
-    }
-    if(req.params.item_id.qty){
-      item.qty = req.params.item_id.qty;
-    }
-    if(req.params.item_id.qtyUnit){
-      item.qtyUnit = req.params.item_id.qtyUnit;
-    }
-    if(req.params.item_id.img){
-      item.img = req.params.item_id.img;
-    }
-    const updatedItem = await item.save();
+    await item.updateOne(req.body);
+    let updatedItem = await db.Item.findById(req.params.item_id);
     return res.status(200).json(updatedItem);
   }catch(err){
     return next(err);
   }
 }
+// /api/items/:item_id
 exports.deleteItem = async function(req, res, next){
   try{
     let foundItem = await db.Item.findById(req.params.item_id);
